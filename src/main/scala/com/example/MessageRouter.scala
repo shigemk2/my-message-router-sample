344358bd7d31b006810484032a53d66fb0fad9bd
shigemk2 <i.am.shige@gmail.com>
2016-11-10 02:02:51 +0900

    Add AlternatingRouter

--- src/main/scala/com/example/MessageRouter.scala
+++ src/main/scala/com/example/MessageRouter.scala
@@ -1,6 +1,29 @@
 package com.example
 
+import akka.actor.Actor.Receive
 import akka.actor._
 
 object MessageRouterDriver extends CompletableApp(20) {
+}
+
+class AlternatingRouter(processor1: ActorRef, processor2: ActorRef) extends Actor {
+  var alternate = 1
+
+  def alternateProcessor() = {
+    if (alternate == 1) {
+      alternate = 2
+      processor1
+    } else {
+      alternate = 1
+      processor2
+    }
+  }
+
+  override def receive: Receive = {
+    case message: Any =>
+      val processor = alternateProcessor()
+      println(s"AlternatingRouter: routing $message to ${processor.path.name}")
+      processor ! message
+      MessageRouterDriver.completedStep()
+  }
 }
